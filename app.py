488fde616b625bedfca66008deeb69449aeed8ee
Aleksander Pres <aleksander.pres98@gmail.com>
2023-04-22 08:13:01 +0200

    app functionality changed

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
-''' Simple flask application '''
-from flask import Flask,render_template, request, redirect, url_for
+from flask import Flask, render_template, request, session, flash, redirect, send_file, url_for
+from pytube import YouTube
+from io import BytesIO
+import os
+
+
+PHOTO_FOLDER = os.path.join("static", "images")
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    ''' Render from on main page. '''
-    return render_template('index.html')
+app.config["UPLOAD_FOLDER"] = PHOTO_FOLDER
+app.config["SECRET_KEY"] = "some_secret_key"
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    ''' Redirect to hello, <name> page. '''
-    name = request.form['name']
-    return redirect(url_for('hello', name=name))
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    yt_logo = os.path.join(PHOTO_FOLDER, "youtube-logo.png")
 
-@app.route('/hello/<name>')
-def hello(name):
-    ''' Render gretings. '''
-    return render_template('hello.html', name=name)
+    if request.method == 'POST':
+        session['link'] = request.form.get('url')
+        try:
+            url = YouTube(session['link'])
+            url.check_availability()
+        except:
+            flash("Wrong URL!", category='danger')
+            return render_template('home.html', youtube_logo=yt_logo)
+        return render_template('download.html', url=url, youtube_logo = yt_logo)
+    return render_template('home.html', youtube_logo=yt_logo)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8001)
-    
+@app.route('/download', methods=['POST', 'GET'])
+def download():
+
+    if request.method == 'POST':
+        url = YouTube(session['link'])
+        buffer = BytesIO()
+        itag = request.form.get('resolution')
+        video = url.streams.get_by_itag(itag)
+        video.stream_to_buffer(buffer)
+        buffer.seek(0)
+        send_file(buffer, as_attachment=True, download_name=f'{url.title}.mp4', mimetype='video/mp4')
+        flash("Download Successful!", category='success')
+
+    return redirect(url_for('home'))
+
+if __name__ == "__main__":
+    app.run(port=8001)
